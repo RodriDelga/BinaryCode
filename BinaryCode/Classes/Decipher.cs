be1e9fe8c633719f09e371b7d5d7df2de4a9171f
Rodrigo Yared Delgado Ochoa <yareddelgado18@gmail.com>
2025-08-25 09:40:56 -0600

    I fixed the error when you put something else 0 or 1 in the decipher code.

--- BinaryCode/Classes/Decipher.cs
+++ BinaryCode/Classes/Decipher.cs
@@ -39,15 +39,39 @@ namespace BinaryCode.Classes
                         chain = chain.Substring(8);
                     }
                 }
-                //for (int i = 0; i < codeArray.Length; i++)
-                //{
-                //    Console.WriteLine($"Arreglo[{i}]={codeArray[i]}");
-                //}
-                total = Transform(codeArray);
-                decipher = (char)total;
-                code += decipher;
+                if (check_code(chain, codeArray))
+                {
+                    total = Transform(codeArray);
+                    decipher = (char)total;
+                    code += decipher;
+                }
+                else
+                {
+                    code = "invalid";
+                    Console.WriteLine("\nWrite a code with just 0 and 1, no characters.");
+                    break;
+                }  
             }
             return code;
+        }
+
+        public bool check_code(string chain, string[] codeArray)
+        {
+            bool result = false;
+            for (int i = 0; i < codeArray.Length; i++)
+            {
+                if (codeArray[i] == "0" || codeArray[i] == "1")
+                {
+                    // Console.WriteLine($"Arreglo[{i}]={codeArray[i]}");
+                    result = true;
+                }
+                else
+                {
+                    Console.WriteLine(codeArray[i]);
+                    break;
+                }
+            }
+            return result;
         }
         public int Transform(string[] codeArray) 
         {
