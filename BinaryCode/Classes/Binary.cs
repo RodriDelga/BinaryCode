b15df7a325923db795dcfa81085e5d3b83d01d02
yared <yared@LAPTOPRODRIGO>
2025-08-24 22:02:54 -0600

    Agregar archivos de proyecto.

---
+++ BinaryCode/Classes/Binary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryCode.Classes
+{
+    public class Binary
+    {
+        public string Cypher(string chain)
+        {
+            string cypherChain, correctChain;
+            char cypher;
+            int numC = 0, character, totalCypher;
+            string resultado = "";
+            while (numC < chain.Length)
+            {
+                cypherChain = "";
+                correctChain = "";
+                cypher = chain[numC];
+                character = (int)(cypher);
+                for (int i = 0; i < 7; i++) 
+                {
+                    totalCypher = character % 2;
+                    character = character / 2;
+                    Console.WriteLine(character);
+                    cypherChain += totalCypher;
+                }
+                cypherChain += "0";
+                for (int i = 7; i >= 0; i--) 
+                { 
+                    correctChain += cypherChain[i];
+                }
+                resultado += correctChain;
+                Console.WriteLine(resultado);
+                numC++;
+            }
+            return resultado;
+        }
+    }
+}
---
+++ BinaryCode/Program.cs
@@ -0,0 +1,7 @@
+﻿using BinaryCode.Classes;
+
+Binary start = new Binary();
+
+string chain = "Hello";
+
+Console.WriteLine(start.Cypher(chain));
